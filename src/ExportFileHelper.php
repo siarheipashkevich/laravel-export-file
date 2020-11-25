86669a07df4edb64010e62474579d1957c3e1304
Sergey Pashkevich <siarheipashkevich@gmail.com>
2020-11-26 00:22:56 +0300

    some changes

--- src/ExportFileHelper.php
+++ src/ExportFileHelper.php
@@ -10,8 +10,8 @@ namespace Esupl\ExportFile;
 class ExportFileHelper
 {
     public const TYPE_FIELD = 'exf_type';
-    public const FORMAT_FIELD = 'exf_type';
-    public const MODE_FIELD = 'exf_type';
+    public const FORMAT_FIELD = 'exf_format';
+    public const MODE_FIELD = 'exf_mode';
 
     public const AUTO_MODE = 'auto';
     public const DOWNLOAD_MODE = 'download';
